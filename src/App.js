63a3ed6ab722eaf5dcb51e1438b2f6b244e3a897
Aman-GITAM <47055062+Aman-GITAM@users.noreply.github.com>
2023-03-13 14:03:18 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import './App.css'
 
-function App() {
+const App = () => {
+
+  const [inputValue, setInputValue] = useState('');
+  const [listValue, setListValue] = useState([]);
+  // console.log(inputValue);
+
+  // Add todo
+  const getInputValue = () => {
+    if (inputValue) {
+      setListValue([...listValue, inputValue]);
+      setInputValue('');
+    }
+  }
+
+  // Delete todo
+  const deleteListItem = (id) => {
+    const updatedList = listValue.filter((elem, idx) => {
+      return idx !== id;
+    })
+    setListValue(updatedList);
+  }
+
+  // Remove all list
+  const removeAll = () => {
+    setListValue([])
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <div className='main-container'>
+
+        <div className='todo-container'>
+
+          <div className='todo-head'>
+            <span>To-Do List</span>
+          </div>
+
+          <div className='todo-input'>
+            <input placeholder='writes your notes...'
+              onChange={(e) => setInputValue(e.target.value)}
+              value={inputValue} />
+            <div className='todo-add'><button onClick={getInputValue}>Add</button></div>
+          </div>
+
+          {
+            listValue.map((item, idx) =>
+              <div className='list-container' key={idx}>
+                <div className='todo-notes'><span>{item}</span></div>
+                <div className='todo-delete'><button onClick={() => deleteListItem(idx)}>delete</button></div>
+              </div>
+            )
+          }
+
+          {
+            listValue.length > 1 ? <div className='todo-remove'>
+              <div className='clear-todo' onClick={removeAll}>
+                <button>Remove All</button>
+              </div>
+            </div> : null
+          }
+
+
+        </div>
+
+      </div>
+    </>
+  )
 }
 
-export default App;
+export default App
